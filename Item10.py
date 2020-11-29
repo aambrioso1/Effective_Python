e92efa61d675cc517715fc6edb2136f85e0f05a9
aambrioso <aambrioso1@gmail.com>
2020-11-28 19:37:57 -0500

    Fix item10.py and modify the README.md.

--- Item10.py
+++ Item10.py
@@ -2,83 +2,211 @@
 Item 10:  Prevent Repetitions with Assignment Expressions (walrus operator := )
 
 """
+
+# Example 1
 fresh_fruit = {
-	'apple': 10,
-	'banana': 1,
-	'lemon': 5,
+    'apple': 10,
+    'banana': 8,
+    'lemon': 5,
 }
 
-def make_cider(n):
-	return 'cider'
 
+# Example 2
 def make_lemonade(count):
-	return 'lemonade'
-
-def slice_bananas(count):
-	return 4*count
-
-def OutOfBananas(Exception):
-	pass
+    print(f'Making {count} lemons into lemonade')
 
 def out_of_stock():
-	'out_of_stock'
+    print('Out of stock!')
+
+count = fresh_fruit.get('lemon', 0)
+if count:
+    make_lemonade(count)
+else:
+    out_of_stock()
+
+
+# Example 3
+if count := fresh_fruit.get('lemon', 0):
+    make_lemonade(count)
+else:
+    out_of_stock()
+
+
+# Example 4
+def make_cider(count):
+    print(f'Making cider with {count} apples')
+
+count = fresh_fruit.get('apple', 0)
+if count >= 4:
+    make_cider(count)
+else:
+    out_of_stock()
+
+
+# Example 5
+if (count := fresh_fruit.get('apple', 0)) >= 4:
+    make_cider(count)
+else:
+    out_of_stock()
+
+
+# Example 6
+def slice_bananas(count):
+    print(f'Slicing {count} bananas')
+    return count * 4
+
+class OutOfBananas(Exception):
+    pass
 
 def make_smoothies(count):
-	return 'smoothies'
+    print(f'Making a smoothies with {count} banana slices')
 
-
-if count := fresh_fruit.get('lemon', 0):
-	print(make_lemonade(count))
-else:
-	print(out_of_stock())
-
-if (count := fresh_fruit.get('apple',0)) >= 4:
-	print(make_cider(count))
-else:
-	print(out_of_stock())
-
-if (count := fresh_fruit.get('pear',0)):
-	print(count)
-else:
-	print('no pears')
-
-print(f'fresh_fruit = {fresh_fruit}')
-
-
-if (count := fresh_fruit.get('banana', 0)) >= 2:
-	pieces = slice_bananas(count)
-else:
-	pieces = 0
+pieces = 0
+count = fresh_fruit.get('banana', 0)
+if count >= 2:
+    pieces = slice_bananas(count)
 
 try:
-	smoothies = make_smoothies(pieces)
+    smoothies = make_smoothies(pieces)
 except OutOfBananas:
-	out_of_stock()
+    out_of_stock()
 
-# Using the walrus operator for implementing a flexible switch/case-like statement
 
-if  (count := fresh_fruit.get('banana', 0) >= 2):
-	pieces = slice_bananas(count)
-	to_enjoy = make_smoothies(pieces)
-elif (count := fresh_fruit.get('apple', 0) >= 4):
-	to_enjoy = make_cider(count)
-elif count := fresh_fruit.get('lemon', 0):
-	to_enjoy = make_lemonade(count)
+# Example 7
+count = fresh_fruit.get('banana', 0)
+if count >= 2:
+    pieces = slice_bananas(count)
 else:
-	to_enjoy = 'nothing'
+    pieces = 0
 
-# Use the walrus operator to write better code do/while loops
+try:
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+
+
+# Example 8
+pieces = 0
+if (count := fresh_fruit.get('banana', 0)) >= 2:
+    pieces = slice_bananas(count)
+
+try:
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+
+
+# Example 9
+if (count := fresh_fruit.get('banana', 0)) >= 2:
+    pieces = slice_bananas(count)
+else:
+    pieces = 0
+
+try:
+    smoothies = make_smoothies(pieces)
+except OutOfBananas:
+    out_of_stock()
+
+
+# Example 10
+print("**********")
+print("Example 10")
+print("**********")
+
+fresh_fruit = {
+    'apple': 3,
+    'banana': 1,
+    # 'lemon': 5,
+}
+
+count = fresh_fruit.get('banana', 0)
+if count >= 2:
+    pieces = slice_bananas(count)
+    to_enjoy = make_smoothies(pieces)
+else:
+    count = fresh_fruit.get('apple', 0)
+    if count >= 4:
+        to_enjoy = make_cider(count)
+    else:
+        count = fresh_fruit.get('lemon', 0)
+        if count:
+            to_enjoy = make_lemonade(count)
+        else:
+            to_enjoy = 'Nothing'
+
+print(f'to_enjoy = {to_enjoy}')
+
+# Example 11
+# Using the walrus operator for implementing a flexible switch/case-like statement
+if (count := fresh_fruit.get('banana', 0)) >= 2:
+    pieces = slice_bananas(count)
+    to_enjoy = make_smoothies(pieces)
+elif (count := fresh_fruit.get('apple', 0)) >= 4:
+    to_enjoy = make_cider(count)
+elif count := fresh_fruit.get('lemon', 0):
+    to_enjoy = make_lemonade(count)
+else:
+    to_enjoy = 'Nothing'
+
+
+# Example 12
+FRUIT_TO_PICK = [
+    {'apple': 1, 'banana': 3},
+    {'lemon': 2, 'lime': 5},
+    {'orange': 3, 'melon': 2},
+]
 
 def pick_fruit():
-	pass
+    if FRUIT_TO_PICK:
+        return FRUIT_TO_PICK.pop(0)
+    else:
+        return []
 
-def make_juice():
-	pass
+def make_juice(fruit, count):
+    return [(fruit, count)]
+
+bottles = []
+fresh_fruit = pick_fruit()
+while fresh_fruit:
+    for fruit, count in fresh_fruit.items():
+        batch = make_juice(fruit, count)
+        bottles.extend(batch)
+    fresh_fruit = pick_fruit()
+
+print(bottles)
+
+
+# Example 13
+# Use the walrus operator to write better code do/while loops
+FRUIT_TO_PICK = [
+    {'apple': 1, 'banana': 3},
+    {'lemon': 2, 'lime': 5},
+    {'orange': 3, 'melon': 2},
+]
+
+bottles = []
+while True:                     # Loop
+    fresh_fruit = pick_fruit()
+    if not fresh_fruit:         # And a half
+        break
+    for fruit, count in fresh_fruit.items():
+        batch = make_juice(fruit, count)
+        bottles.extend(batch)
+
+print(bottles)
+
+
+# Example 14
+FRUIT_TO_PICK = [
+    {'apple': 1, 'banana': 3},
+    {'lemon': 2, 'lime': 5},
+    {'orange': 3, 'melon': 2},
+]
 
 bottles = []
 while fresh_fruit := pick_fruit():
-	for fruit, count in fresh_fruit.items():
-		batch = make_juice(fruit, count)
-		bottles.extend(batch)
+    for fruit, count in fresh_fruit.items():
+        batch = make_juice(fruit, count)
+        bottles.extend(batch)
 
-print(to_enjoy)
+print(bottles)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Effective_Python
-Code and Notes from Brett Slatkin's Effective Python (2nd Edition)
+Code and notes from Brett Slatkin's Effective Python (2nd Edition)
+
+This book offers only lots of insight into writing Python code.   But more importantly, it gives insight
+into how to write clear code.
+
+All the code, along with some nice improvements, for Slatkin's book can be found here:
+
+https://github.com/bslatkin/effectivepython/tree/master/example_code
