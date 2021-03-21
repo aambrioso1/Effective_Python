0a03022e85961e78779b16497afdb1cc44622ead
aambrioso <aambrioso1@gmail.com>
2021-03-21 13:37:22 -0400

    complete item 54

--- Item54.py
+++ Item54.py
@@ -3,6 +3,13 @@ Item 54: Use Lock to Prevent Data Races in Threads
 
 """
 
+"""
+The GIL does not protect against data races between threads.   The program must do this.
+Data structures can be corrupted if you allow multiple threads to modify the same objects
+with mutual-exclusion locks (mutexes)
+The Lock class from the threading built-in module will prevent this kind of data corruption.
+"""
+
 #!/usr/bin/env PYTHONHASHSEED=1234 python3
 
 # Reproduce book environment
@@ -37,97 +44,102 @@ def close_open_files():
 atexit.register(close_open_files)
 
 
-# Example 1
-def factorize(number):
-    for i in range(1, number + 1):
-        if number % i == 0:
-            yield i
+# Example 1:  We implement a counter class to count sensor measurement for sensor running on separate
+# worker threads.
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def increment(self, offset):
+        self.count += offset
 
 
-# Example 2
-import time
+# Example 2:  The function monitors a group of sensor and increments to total count 
+# each time it takes a reading from one.
+def worker(sensor_index, how_many, counter):
+    # I have a barrier in here so the workers synchronize
+    # when they start counting, otherwise it's hard to get a race
+    # because the overhead of starting a thread is high.
+    BARRIER.wait()
+    for _ in range(how_many):
+        # Read from the sensor
+        # Nothing actually happens here, but this is where
+        # the blocking I/O would go.
+        counter.increment(1)
 
-numbers = [2139079, 1214759, 1516637, 1852285]
-start = time.time()
 
-for number in numbers:
-    list(factorize(number))
-
-end = time.time()
-delta = end - start
-print(f'Took {delta:.3f} seconds')
-
-
-# Example 3
+# Example 3:  We run 5 workers in parallel.
+from threading import Barrier
+BARRIER = Barrier(5)
 from threading import Thread
 
-class FactorizeThread(Thread):
-    def __init__(self, number):
-        super().__init__()
-        self.number = number
-
-    def run(self):
-        self.factors = list(factorize(self.number))
-
-
-# Example 4
-start = time.time()
+how_many = 10**5
+counter = Counter()
 
 threads = []
-for number in numbers:
-    thread = FactorizeThread(number)
-    thread.start()
+for i in range(5):
+    thread = Thread(target=worker,
+                    args=(i, how_many, counter))
     threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+expected = how_many * 5
+found = counter.count
+print(f'Counter should be {expected}, got {found}')
+
+
+# Example 4:   This line of code operates as shown in example 5.   Python threads can be interupted
+# between the operation show in example 5.
+counter.count += 1
 
 
 # Example 5
-for thread in threads:
-    thread.join()
-
-end = time.time()
-delta = end - start
-print(f'Took {delta:.3f} seconds')
+value = getattr(counter, 'count')
+result = value + 1
+setattr(counter, 'count', result)
 
 
-# Example 6
-import select
-import socket
-
-def slow_systemcall():
-    select.select([socket.socket()], [], [], 0.1)
-
-
-# Example 7
-start = time.time()
-
-for _ in range(5):
-    slow_systemcall()
-
-end = time.time()
-delta = end - start
-print(f'Took {delta:.3f} seconds')
+# Example 6:   This example shows what can happen when threads are interrupted.
+# Running in Thread A
+value_a = getattr(counter, 'count')
+# Context switch to Thread B
+value_b = getattr(counter, 'count')
+result_b = value_b + 1
+setattr(counter, 'count', result_b)
+# Context switch back to Thread A
+result_a = value_a + 1
+setattr(counter, 'count', result_a)
 
 
-# Example 8
-start = time.time()
+# Example 7:  We use the Lock class and a with statement to adquire and release the lock.
+from threading import Lock
 
-threads = []
-for _ in range(5):
-    thread = Thread(target=slow_systemcall)
-    thread.start()
-    threads.append(thread)
+class LockingCounter:
+    def __init__(self):
+        self.lock = Lock()
+        self.count = 0
+
+    def increment(self, offset):
+        with self.lock:
+            self.count += offset
 
 
-# Example 9
-def compute_helicopter_location(index):
-    pass
+# Example 8:  Now the code works as expected.
+BARRIER = Barrier(5)
+counter = LockingCounter()
 
 for i in range(5):
-    compute_helicopter_location(i)
+    thread = Thread(target=worker,
+                    args=(i, how_many, counter))
+    threads.append(thread)
+    thread.start()
 
 for thread in threads:
     thread.join()
 
-end = time.time()
-delta = end - start
-print(f'Took {delta:.3f} seconds')
+expected = how_many * 5
+found = counter.count
+print(f'Counter should be {expected}, got {found}')
