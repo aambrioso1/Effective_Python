2517a0b1a85dbefb16f1ebaaef5928107fdcacce
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2021-05-09 11:37:12 -0400

    modify item76

--- Item76.py
+++ Item76.py
@@ -1,5 +1,20 @@
 """
-Item : 
+Item 76: Verify Related Behaviors in TestCase SubClasses
+
+This item required different files.   Look for the following files in the Item76 folder:
+
+utils.py
+utils_test.py
+assert_test.py
+data_driven_test.py
+
+
+You can create tests by subclasses the TestCase class from the unittest built-int module.
+Define one method per behavior.    Test methods must start with the word test.
+
+Various helper methdos defined by the TestCase class to confirm expected behaviors.
+
+Consider writing data-driven tests using the subTest helper method to reduce boilerplate.
+
 
 """
-
