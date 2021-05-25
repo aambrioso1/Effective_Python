19423b64aeeffa83a4550e232ef766446c0bdf07
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2021-05-25 17:16:04 -0400

    complete Item81

---
+++ Item81/top_n.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env PYTHONHASHSEED=1234 python3
+
+
+import tracemalloc
+
+tracemalloc.start(10)                      # Set stack depth
+time1 = tracemalloc.take_snapshot()        # Before snapshot
+
+import waste_memory
+
+x = waste_memory.run()                     # Usage to debug
+time2 = tracemalloc.take_snapshot()        # After snapshot
+
+stats = time2.compare_to(time1, 'lineno')  # Compare snapshots
+for stat in stats[:3]:
+    print(stat)
